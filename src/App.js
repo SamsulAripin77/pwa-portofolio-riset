024977a4364bc8bfe400bbc5bf68a222f2d1a3bf
SamsulAripin77 <arifins192@gmail.com>
2021-02-05 18:20:04 +0700

    add landing unfinish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import SertifikasiCreate from './pages/sertifikasi/SertifikasiCreate'
-import Landing from './pages/Landing'
+import Landing from './Landing'
 import Login from './pages/auth/Login'
 import UserDashboard from './pages/auth/UserDashboard'
 import AdminDasboard from './pages/auth/AdminDasboard'
@@ -9,10 +9,10 @@ import AdminDasboard from './pages/auth/AdminDasboard'
 function App() {
   return (
       <BrowserRouter>
+      <Route exact path="/" component={Landing}></Route>
       <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
         <div className="relative py-3 sm:max-2-xl sm:mx-auto">
         <Switch>
-        <Route exact path="/" component={Landing}></Route>
         <Route exact path="/login" component={Login}></Route>
         <Route  path="/admin" component={AdminDasboard}></Route>
         <Route  path="/user" component={UserDashboard}></Route>
--- src/components/Button.js
+++ src/components/Button.js
@@ -5,5 +5,4 @@ function Button(props) {
         <button type={props.type}  className={`button bg-${props.color}-500 flex justify-center items-center w-full text-white px-4 py-3 rounded-md focus:outline-none`}>{props.label}</button>
     );
 }
-
 export default Button;
